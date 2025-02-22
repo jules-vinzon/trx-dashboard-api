8c1579aae2a49c45f114c06792152cc2e89ded6c
Jules <rose.vinzon@paynamics.net>
2025-02-23 02:23:42 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+
+import express, { json } from "express";
+import morgan from "morgan";
+import usersRoutes from "./src/routes/users-routes.js";
+import transactionRoutes from "./src/routes/transaction-routes.js";
+import connectDB from './src/configs/mongodb.js';
+import { cronJob } from "./src/configs/cronJob.js";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(json());
+app.use(morgan("dev"));
+
+app.use("/api/users", usersRoutes);
+app.use("/api/transactions", transactionRoutes);
+
+app.listen(port, () => {
+
+
+    cronJob();
+
+    connectDB().then(() => {
+        console.log('CONNECTED TO MONGODB');
+    }).catch(err => console.error(err));
+
+    console.log(`SERVER RUNNING ON http://localhost:${port}`);
+});
